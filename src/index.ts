5c86124dbe9b4719a301af13a7ee476f183b2829
shivweb <sp10595@gmail.com>
2026-03-19 15:54:52 +0530

    added some routes

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import type { Application, Request, Response } from "express";
 import "dotenv/config";
 import cors from "cors";
+
 const app: Application = express();
 const PORT = process.env.PORT || 7000;
 
@@ -10,13 +11,18 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+// ✅ Default route
 app.get("/", (req: Request, res: Response) => {
-  return res.send("It's working 🙌");
+  return res.send("It's working good 🙌");
 });
 
-
-
+// ✅ NEW CI/CD TEST ROUTE
+app.get("/cicd-test", (req: Request, res: Response) => {
+  return res.json({
+    message: "CI/CD is working 🚀",
+    timestamp: new Date(),
+    version: "v1.1",
+  });
+});
 
 app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
-
-
